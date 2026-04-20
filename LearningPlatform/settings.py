2a7fe5c40ef6b100679f55997d2415d5d9b6377e
Rakesh N <nraki522@gmail.com>
2026-04-20 23:18:44 +0530

    fixed the database issues

--- LearningPlatform/settings.py
+++ LearningPlatform/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'LearningPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'learningplatforms',
-        'HOST':'localhost',
-        'USER':'root',
-        'PASSWORD':'password123'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
